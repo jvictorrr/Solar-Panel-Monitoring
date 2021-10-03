41ebe41e37f583c5be5e331f367e4eafbea156e0
João Victor Rodrigues Ramalho <joao.ramalho@ee.ufcg.edu.br>
2021-10-03 15:11:21 -0300

    Inclusão LCD

--- Solar_panel_monitoring/Solar_panel_monitoring.ino
+++ Solar_panel_monitoring/Solar_panel_monitoring.ino
@@ -1,4 +1,6 @@
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 // Medição de Corrente 
 const int currentPin = A1;
 int sensitivity = 66;
@@ -24,17 +26,28 @@ float Vlight = 0.0;
 int Temp = 0;
 
 void setup() {
-  Serial.begin(9600);          
+  Serial.begin(9600);      
+  lcd.begin(16, 2);    
 
 }
 void loop(){
 
+   lcd.clear();
+   lcd.setCursor(0, 0);
+
+   
   // Leitura da tensão 
   Voltage_divide_input= analogRead(voltagePin);
    Voltage_divide_value = Voltage_divide_input * (5.0 / 1023.0);
   finalVoltage = (Voltage_divide_value*referenceVoltage)/Rf;
   Serial.print("Voltage = ");
   Serial.println(finalVoltage,1);
+  lcd.print("V= ");
+  lcd.setCursor(2, 0);
+  lcd.print(finalVoltage,1);
+  lcd.setCursor(6, 0);
+  lcd.print("V");  
+  
 
   // Leitura da Corrente 
   adcValue = analogRead(currentPin);
@@ -43,10 +56,27 @@ void loop(){
   Serial.print("Current = ");
   Serial.println(currentValue,3);
 
+  lcd.setCursor(8, 0);
+  lcd.print("i= ");
+lcd.setCursor(10, 0);
+lcd.print(currentValue,2);
+lcd.setCursor(14, 0);
+lcd.print("A");
+
   // Leitura da Luminosidade
   Vlight = analogRead(A3);  
   Serial.print("Light = ");         
   Serial.println(Vlight*100/1023);
+  
+lcd.setCursor(0, 1);
+lcd.print("Sun= ");
+lcd.setCursor(4, 1);
+lcd.print(Vlight*100/1023,1);
+lcd.setCursor(8, 1);
+lcd.print("%");
+
+
+  
 
   //Leitura da Temperatura
   Temp = analogRead(A5);  
@@ -54,5 +84,15 @@ void loop(){
   Serial.print("Temp = ");  
   Serial.println(TempCel);
 
-    delay(1000);
+lcd.setCursor(10, 1);
+lcd.print("T=");
+lcd.setCursor(12, 1);
+lcd.print(TempCel,0);
+lcd.setCursor(14, 1);
+lcd.write(B11011111);
+lcd.setCursor(15, 1);
+lcd.print("C");
+
+
+    delay(100);
 }
